aa3873881a9542d2c134d9acf919ed6f23be5379
eric07109 <eric07109@gmail.com>
2020-11-18 15:09:38 +0800

    highway network implementations

--- highway.py
+++ highway.py
@@ -2,7 +2,27 @@
 # -*- coding: utf-8 -*-
 
 ### YOUR CODE HERE for part 1d
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.utils
 
+class Highway(nn.Module):
+    def __init__(self, word_embedding_size, dropout_rate = 0.2):
+        self.word_embedding_size = word_embedding_size
+        self.projection_weights = nn.Linear(self.word_embedding_size, self.word_embedding_size, bias=True)
+        self.gate_weights = nn.Linear(self.word_embedding_size, self.word_embedding_size, bias=True)
+        self.dropout = nn.Dropout(dropout_rate)
+
+    def forward(self, convolution_outputs):
+        # batch operation
+        highway_outputs = []
+        for conv_output in convolution_outputs:
+            x_proj = nn.ReLU(self.projection_weights(convolution_outputs))
+            x_gate = nn.Sigmoid(self.gate_weights(convolution_outputs))
+            x_highway = (x_gate * x_proj) + ((1 - x_gate) * x_proj)
+            highway_outputs.append(x_highway)
+        return highway_outputs
 
 ### END YOUR CODE 
 
